0dd9179405fd7842aaa37e9c720d46de4c9e11e3
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2021-06-08 14:24:18 -0300

    - Aula "Destructors in C++" concluida

--- BMOAulasCpp16ConstructorsInCpp/main.cpp
+++ BMOAulasCpp16ConstructorsInCpp/main.cpp
@@ -22,21 +22,35 @@ public:
     
     //CONSTRUTOR: É um metodo especial(sem tipo de retorno, e com mesmo nome da classe) executado na instanciacao:
     Entidade() { //Esse construtor já resolve o problema, pois inicializa com algum valor os atributos
+        std::cout << "Entidade Construida!" << std::endl;
         X=0.0f;
         Y=0.0f;
     }
     Entidade(float x, float y) { // Esse construtor é a solucao ideal, pois da a opcao de escolher os valores.
+        std::cout << "Entidade Construida!" << std::endl;
         X=x;
         Y=y;
     }
+    
+    //DESTRUTOR: É o contrario do construtor, e é usando para liberar espaco na memoria.
+    ~Entidade() { //Este metodo é chamado sempre que o Escopo em que ele foi criado[nesse caso é o metodo main()] chega ao fim
+        std::cout << "Entidade Destruida!" << std::endl;
+        return;
+    }
 };
+
+void Funcao() {
+    Entidade e3; //Aqui a entidade foi Contruida dentro do escopo dessa funcao
+    e3.Print();
+} //Aqui a entidade foi Destruida, pois o escopo em que ela foi criada se encerra nessa linha por conta do digito '}'
+
 
 class Log {
 public:
     Log()=delete; //Isso obriga o desenvolvedor a criar um contrutor, ja que deletamos o construtor padrao dessa classe.
     
     static void Escrever(){
-        // insert code here...
+        std::cout << "Fazendo algo no metodo estatico." << std::endl;
     }
 };
 
@@ -69,6 +83,12 @@ int main(int argc, const char * argv[]) {
     Log::Escrever();
     //Log l; // Essa linha da erro, pois na classe esta escrito "Log()=delete;", que deleta o construtor padrao.
     
+    std::cout << " ----------- Usando Destrutor ----------- " << std::endl;
+    e2.~Entidade(); //Isso é como se destroi uma instancia Manualmente.
+    //Obs: Ao chegar no fim do escopo da funcao main(), e2 sera novamente destruida, pois a Destruicao ocorre autom no fim do escopo
+    
+    Funcao();
+    //Ao chegar nessa linha, tudo o que foi construido dentro do escopo da funcao acima ja tera sido destruido automaticamente
     
     std::cout << "Hello, World!\n";
     return 0;
